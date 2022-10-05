124859f2dfa5f42cfdeb6a87ff46afdcd3ca8f37
Thi anh thu Le <thle@c1r1p9.hive.fi>
2022-10-05 11:03:10 +0300

    normed

--- includes/lemin.h
+++ includes/lemin.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   lem-in.h                                           :+:      :+:    :+:   */
+/*   lemin.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: thle <thle@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 18:06:12 by thule             #+#    #+#             */
-/*   Updated: 2022/10/05 10:46:41 by itkimura         ###   ########.fr       */
+/*   Updated: 2022/10/05 11:02:48 by thle             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,8 @@ typedef enum e_bool
 	TRUE,
 }	t_bool;
 
-enum{
+enum
+{
 	COMMENT,
 	ROOM,
 	START,
@@ -50,14 +51,14 @@ typedef struct s_room
 /* adjacency matrix */
 typedef struct s_info
 {
-	int		quantity_of_ants;
-	int		quantity_of_rooms;
-	char	*start_room;
-	char	*end_room;
-	char	*line;
-	t_room	*room_head;
-	t_room	**hash_table;
-}			t_info;
+	int			quantity_of_ants;
+	int			quantity_of_rooms;
+	char		*start_room;
+	char		*end_room;
+	char		*line;
+	t_room		*room_head;
+	t_room		**hash_table;
+}				t_info;
 
 /* link-room structure */
 typedef struct s_link_room
@@ -67,35 +68,36 @@ typedef struct s_link_room
 }						t_link_room;
 
 /* read_line.c */
-e_bool	read_line(t_info *info);
-e_bool	get_ants(t_info *info, int type, int *stage);
-int		type_of_line(char *str);
-e_bool	path_to_each_stage(t_info *info, int type, int *stage);
+t_bool		read_line(t_info *info);
+t_bool		get_ants(t_info *info, int type, int *stage);
+int			type_of_line(char *str);
+t_bool		path_to_each_stage(t_info *info, int type, int *stage);
 
 /* create_room.c */
-char	*get_room_name(char *line);
-e_bool	get_rooms(t_info *info, int type);
-e_bool	create_new_room(t_room **room, t_info *info);
+char		*get_room_name(char *line);
+t_bool		get_rooms(t_info *info, int type);
+t_bool		create_new_room(t_room **room, t_info *info);
 
 /* create_link */
 
 /* hash_table.c */
-e_bool	init_hash_table(t_info *info);
-e_bool	hash_table_appending(t_info *info, t_room *list);
-e_bool	create_hash_table(t_info *info);
-int		hash(char *str, int size);
+t_bool		init_hash_table(t_info *info);
+t_bool		hash_table_appending(t_info *info, t_room *list);
+t_bool		create_hash_table(t_info *info);
+t_room		*hash_table_lookup(t_room **hash_table, char *str, int size);
+int			hash(char *str, int size);
 
 /* lem-in.c */
-e_bool	lem_in(void);
-e_bool	check_xy(char *line);
-e_bool	check_digit_and_space(char *line);
-e_bool	init_info(t_info *info);
+t_bool		lem_in(void);
+t_bool		check_xy(char *line);
+t_bool		check_digit_and_space(char *line);
+t_bool		init_info(t_info *info);
 
 /* utils.c */
-void	free_rooms(t_room **head);
-void	print_room(t_room *room);
-void	print_info(t_info *info);
-void	error(char *error);
-void	print_hash_table(t_info *info);
+void		free_rooms(t_room **head);
+void		print_room(t_room *room);
+void		print_info(t_info *info);
+void		error(char *error);
+void		print_hash_table(t_info *info);
 
 #endif
